d28eba722218d8b138ea10d866bc7ee2970fe7c3
lenovo-20 <lenovo-20@LAPTOP-VD6MJEJB>
2022-09-18 19:55:19 -0300

    Atualização

--- CursoJava/src/br/com/curso/CursoJava.java
+++ CursoJava/src/br/com/curso/CursoJava.java
@@ -11,7 +11,7 @@ public class CursoJava {
 	     n1=sc.nextLine();
 	     System.out.print("Digite o seu segundo nome: ");
 	     n2=sc.nextLine();
-	     System.out.println("O seu primeiro e segundo nome é: "+n1+" "+n2);
+	     System.out.println("O seu primeiro e segundo nome é: "+(n1+" "+n2));
 	     System.out.println();
 	     System.out.println(" * Primeiro programa em Java :) ");
 	     System.out.println(" * Ola mundo !!!!!!!!! ");
